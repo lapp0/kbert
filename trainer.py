6b8b990f906da47538e368541eea1ce5a902b1be
Andrew Lapp <andrew@github.rew.la>
2025-01-04 13:23:15 -0500

    update mlm lerp params - was incorrect

--- trainer.py
+++ trainer.py
@@ -210,7 +210,7 @@ def train(args, model_config):
     final_keep_replace_prob = torch.tensor(0.015, device='cuda')
 
     lerp_mask_prob = LerpTensor(start_val=0.2, end_val=0.12, precision=0.01)
-    lerp_keep_replace_prob = LerpTensor(start_val=0.15, end_val=0.015, precision=0.015)
+    lerp_keep_replace_prob = LerpTensor(start_val=0.075, end_val=0.015, precision=0.015)
     lerp_sw_size = LerpTensor(start_val=1024, end_val=args.max_length, precision=128)
 
 
